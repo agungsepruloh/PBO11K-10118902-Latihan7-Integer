3c333991d4b01888a1ae09288d74505ccd17a6a8
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 18:54:18 +0700

    Initial commit

---
+++ src/com/pbo/PBO11K10118902Latihan7Integer.java
@@ -0,0 +1,30 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * Deskripsi Program : Program ini berisi macam-macam tipe data penampung nilai bilangan bulat
+ *
+ */
+
+public class PBO11K10118902Latihan7Integer {
+
+    public static void main(String[] args) {
+
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+    }
+}
